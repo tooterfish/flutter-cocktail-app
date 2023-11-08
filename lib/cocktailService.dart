f7dee4e49be20df569d8233ad740181c51a996af
Harry Fleming <email@harryfleming.net>
2023-11-08 18:00:41 +0000

    initial get behaviour implemented

---
+++ lib/cocktailService.dart
@@ -0,0 +1,26 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Cocktail {
+  final String name;
+
+  const Cocktail({
+    required this.name,
+  });
+}
+
+class CocktailService {
+  Future<Cocktail> getCocktail() async {
+    final response = await http.get(
+        Uri.parse("https://www.thecocktaildb.com/api/json/v1/1/random.php"));
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final drinkData = data['drinks'][0];
+      final cocktail = Cocktail(name: drinkData['strDrink']);
+      return cocktail;
+    } else {
+      throw Exception('HTTP Failed');
+    }
+  }
+}
